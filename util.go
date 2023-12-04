020e1999407fd4a4036a8718852df8fcf724dec5
Jake Fournier <madjake@gmail.com>
2023-12-03 17:18:56 -0800

    Day 3. A couple false starts but once I switch to regex it was pretty easy.

---
+++ util.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+)
+
+type numberMatch struct {
+	num int
+	x   int
+	y   int
+}
+
+type runeMatch struct {
+	c rune
+	x int
+	y int
+}
+
+func matchNumbers(lines []string) []numberMatch {
+	re := regexp.MustCompile(`\d+`)
+
+	numbers := []numberMatch{}
+
+	for y, line := range lines {
+		matches := re.FindAllStringIndex(line, -1)
+
+		for _, match := range matches {
+			num, _ := strconv.Atoi(line[match[0]:match[1]])
+			numbers = append(numbers, numberMatch{
+				num: num,
+				x:   match[0],
+				y:   y,
+			})
+		}
+	}
+
+	return numbers
+}
+
+func matchRunes(r rune, lines []string) []runeMatch {
+	runes := []runeMatch{}
+
+	for y, line := range lines {
+		for x, c := range line {
+			if c == r {
+				runes = append(runes, runeMatch{
+					c: c,
+					y: y,
+					x: x,
+				})
+			}
+		}
+	}
+
+	return runes
+}
